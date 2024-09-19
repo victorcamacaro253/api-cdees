5ec616d1ca4f4a8d61156199be3c6e6eb2a675bb
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-18 23:44:52 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import express, { json } from 'express';
 import empleadosRoutes from './routes/EmpleadosRoutes.js';
 import administradoresRoutes from './routes/administradoresRoutes.js'
-
+import bienesRoutes from './routes/bienesRoutes.js'
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(json());
 app.disable('x-powered-by')
 
+app.use(helmet());
+
 app.use('/api',empleadosRoutes);
 
 app.use('/api2',administradoresRoutes);
+
+app.use('/api3',bienesRoutes);
 
 
 const PORT = process.env.PORT ?? 3001
