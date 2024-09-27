7d192745614e9a663befee2dc86589ba45ae04ff
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-26 22:28:33 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,16 +4,43 @@ import administradoresRoutes from './routes/administradoresRoutes.js'
 import bienesRoutes from './routes/bienesRoutes.js'
 import helmet from 'helmet';
 import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser'; // <-- Agrega esto
+import csrf from 'csurf'; // <-- Agrega esto también
+
+
+// Get the current file's directory (equivalent to __dirname in CommonJS)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
 app.use(cors())
+// Añadir protección CSRF con cookies
+const csrfProtection = csrf({ cookie: true });
+// Middleware para procesar cookies
+app.use(cookieParser());
+
+
 app.use(json());
 app.disable('x-powered-by')
 
 app.use(helmet());
 
-app.use('/api',empleadosRoutes);
+app.use('/uploads',express.static(path.join(__dirname,'uploads')));
+
+
+// Generar y enviar el token CSRF en una ruta
+app.get('/csrf-token', csrfProtection, (req, res) => {
+    // Envía el token CSRF en una cookie llamada 'XSRF-TOKEN'
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.json({ csrfToken: req.csrfToken() });
+  });
+  
+
+app.use('/api',csrfProtection,empleadosRoutes);
 
 app.use('/api2',administradoresRoutes);
 
