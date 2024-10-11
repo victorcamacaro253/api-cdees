a2f3e2dcde19965cfe218af2dad839d924f28583
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-11 00:04:22 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import express, { json } from 'express';
 import empleadosRoutes from './routes/EmpleadosRoutes.js';
 import administradoresRoutes from './routes/administradoresRoutes.js'
 import bienesRoutes from './routes/bienesRoutes.js'
+import empresaRoutes from './routes/empresaRoutes.js'
 import helmet from 'helmet';
 import cors from 'cors'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser'; // <-- Agrega esto
 import csrf from 'csurf'; // <-- Agrega esto tam
+import compression from 'compression';
+
 
 // Get the current file's directory (equivalent to __dirname in CommonJS)
 const __filename = fileURLToPath(import.meta.url);
@@ -30,6 +33,7 @@ app.use(helmet());
 
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 
+app.use(compression())
 
 // Generar y enviar el token CSRF en una ruta
 app.get('/csrf-token', csrfProtection, (req, res) => {
@@ -39,12 +43,13 @@ app.get('/csrf-token', csrfProtection, (req, res) => {
   });
   
 
-app.use('/api',empleadosRoutes);
+app.use(empleadosRoutes);
 
-app.use('/api2',administradoresRoutes);
+app.use(administradoresRoutes);
 
-app.use('/api3',bienesRoutes);
+app.use(bienesRoutes);
 
+app.use(empresaRoutes)
 
 const PORT = process.env.PORT ?? 3001
 
